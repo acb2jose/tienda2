f7856499fac70678d42b2127eed22a48b9b7fd4d
jose <joseacemedina@gmail.com>
2017-08-02 15:49:00 +0200

    primer commit

--- src/app/app-general.service.ts
+++ src/app/app-general.service.ts
@@ -8,6 +8,28 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AppGeneralService {
 
-  constructor() { }
+constructor(private _http: Http) { }
+
+getService(url: string)
+{
+ return this._http.get(url).map(res=>res.json());
+}
+
+postService(url: string,body)
+{
+  var _header=new Headers();
+      _header.append('Content-Type','application/json');
+  return this._http.post(url,body,{headers:_header}).map(res=>res.json());
+}
+
+postWithHeadAutentication(url:string,body,autenticationHead)
+{
+  var _header=new Headers();
+      _header.append('Content-Type','application/json');
+      _header.append('authorization',autenticationHead); 
+      
+  return this._http.post(url,body,{headers:_header}).map(res=>res.json());
+
+}
 
 }
